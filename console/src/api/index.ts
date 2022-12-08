c9ddd34557102976e2721cdc8135d7ba5019373d
ashutoshxhq <mail.ashutoshhq@gmail.com>
2022-12-08 13:25:31 +0530

    updated domain

--- console/src/api/index.ts
+++ console/src/api/index.ts
@@ -4,7 +4,7 @@ import axios from "axios"
 export const faaslyApi = (accessToken: string) => {
 
     return axios.create({
-        baseURL: import.meta.env.VITE_APP_BACKEND_URL || 'https://dev.api.faasly.com/',
+        baseURL: import.meta.env.VITE_APP_BACKEND_URL || 'https://api.faasly.dev/',
         timeout: 10000,
         headers: { 'Authorization': `Bearer ${accessToken}` }
     });
