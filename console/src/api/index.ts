d2b7633e04112c62dbf36d6ed79afca1b3b3ff4a
ashutoshxhq <mail.ashutoshhq@gmail.com>
2022-12-08 10:48:55 +0530

    updated vars

--- console/src/api/index.ts
+++ console/src/api/index.ts
@@ -4,7 +4,7 @@ import axios from "axios"
 export const faaslyApi = (accessToken: string) => {
 
     return axios.create({
-        baseURL: import.meta.env.REACT_APP_BACKEND_URL || 'https://dev.api.faasly.com/',
+        baseURL: import.meta.env.VITE_APP_BACKEND_URL || 'https://dev.api.faasly.com/',
         timeout: 10000,
         headers: { 'Authorization': `Bearer ${accessToken}` }
     });
