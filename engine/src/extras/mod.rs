e1aa7cf1006730ec87223a71eda6b82bb1f16db1
ashutoshxhq <mail.ashutoshhq@gmail.com>
2022-12-08 12:41:37 +0530

    removed scope and update infra files

--- engine/src/extras/mod.rs
+++ engine/src/extras/mod.rs
@@ -5,7 +5,6 @@ pub mod types;
 #[derive(Debug, Serialize, Deserialize)]
 pub struct TokenResponse {
     pub token_type: String,
-    pub scope: String,
     pub access_token: String,
     pub expires_in: u64,
 }
