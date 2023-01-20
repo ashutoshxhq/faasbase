f33f89667bcc5f0cb333e7cb2798f648d992ca13
Ankit Raj <2019ugcs001@nitjsr.ac.in>
2023-01-20 08:12:01 +0530

    readme app and func

--- console/src/pages/Applications/Application/Tabs/ApplicationOverview.tsx
+++ console/src/pages/Applications/Application/Tabs/ApplicationOverview.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { useAuth0 } from '@auth0/auth0-react';
+import { useQuery } from '@tanstack/react-query'
+import { useParams } from 'react-router-dom';
+import { getApplication } from '../../../../api/applications';
+import ReactMarkdown from 'react-markdown'
 
 function ApplicationsOverview() {
+
+  const { applicationId } = useParams();
+  const { getAccessTokenSilently } = useAuth0();
+  const query = useQuery([`applications-${applicationId}`, { getAccessTokenSilently, applicationId }], getApplication);
   return (
-    <div>ApplicationsOverview</div>
+    <ReactMarkdown children={query?.data?.data?.data?.readme} />
   )
 }
 
--- console/src/pages/Functions/Function/Tabs/FunctionOverview.tsx
+++ console/src/pages/Functions/Function/Tabs/FunctionOverview.tsx
@@ -1,11 +1,15 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { Box, Button, Stack, Text } from '@chakra-ui/react'
+import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown'
+import { getFunction } from '../../../../api/functions';
 
 const FunctionOverview = () => {
   const { functionId } = useParams();
   const {getAccessTokenSilently} = useAuth0();
 
+  const query = useQuery([`functions-${functionId}`,{ getAccessTokenSilently, functionId }], getFunction)
   return (
     <Box
       display={"flex"}
@@ -14,42 +18,7 @@ const FunctionOverview = () => {
       flexWrap={"wrap"}
       gap={"30px"}
     >
-      <Text fontSize="lg" fontWeight="medium">
-        Connected Applications
-      </Text>
-      {[].map((application: any, index: number) => (
-        <Box 
-          key = {index}
-          as="section" 
-          borderRadius={8}
-          bg={"#1e1e1e"}
-          boxShadow={"2xl"}
-          border={"solid 0px #424242fd"}
-          px={8}
-          py={6}
-          width={"calc(100%)"}
-          _hover={{ background: "#1e1e1e" }}
-        >
-        <Stack
-          // px={8}
-          direction={{ base: "column", sm: "row" }}
-          justify="space-between"
-          alignItems={"center"}
-        >
-          <Box>
-            <Text fontSize="lg" fontWeight="medium">
-              {application.name}
-            </Text>
-            <Text color="muted" fontSize="sm">
-              {application.description}
-            </Text>
-          </Box>
-          <Stack direction="row">
-            <Button onClick={()=>{}} variant="solid" bgGradient='linear(to-r, orange.500, orange.600)' _hover={{ backgroundColor: "orange.500" }} _active={{ backgroundColor: "orange.500" }}>Open App</Button>
-          </Stack>
-        </Stack>
-        </Box>))
-      }
+      <ReactMarkdown children={query?.data?.data?.data?.readme} />
     </Box>
   )
 }
