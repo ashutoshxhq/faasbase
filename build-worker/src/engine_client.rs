4d185eb922e73abb38858cc894f2bd8e382d3e6a
ashutoshxhq <mail.ashutoshhq@gmail.com>
2023-03-01 13:15:26 +0530

    added new infra setup

--- build-worker/src/engine_client.rs
+++ build-worker/src/engine_client.rs
@@ -1,6 +1,6 @@
 // write a function to call engine service application build update api
 
-use crate::types::{Error, FaasbaseError, UpdateApplicationBuild, WorkerPingPayload};
+use crate::types::{Error, FaasbaseError, UpdateApplicationBuild};
 
 pub async fn update_application_build(
     application_build_id: String,
@@ -17,38 +17,6 @@ pub async fn update_application_build(
         .put(&url)
         .json(&application_build)
         .header("Authorization", format!("Bearer {}", access_token))
-        .send()
-        .await
-        .map_err(|e| {
-            FaasbaseError::new(
-                "ENGINE_SERVICE_ERROR".to_string(),
-                format!("Error while calling engine service: {}", e),
-                500,
-            )
-        })?;
-    if response.status().is_success() {
-        Ok(())
-    } else {
-        Err(FaasbaseError::new(
-            "ENGINE_SERVICE_ERROR".to_string(),
-            format!(
-                "Error while calling engine service: {}",
-                response.text().await?
-            ),
-            500,
-        ))
-    }
-}
-
-pub async fn worker_ping(data: WorkerPingPayload) -> Result<(), Error> {
-    let client = reqwest::Client::new();
-    let url = format!(
-        "{}/worker-ping",
-        std::env::var("ENGINE_SERVICE_URL").unwrap()
-    );
-    let response = client
-        .post(&url)
-        .json(&data)
         .send()
         .await
         .map_err(|e| {
